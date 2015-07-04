4e5ff540e4b0adfdc7a32ee3849bd98606bbccf5
mngy <michaelng@outlook.co.nz>
2015-07-04 15:49:01 +1200

    Updated JSON Object

--- php/spend.php
+++ php/spend.php
@@ -1,7 +1,20 @@
 <?php
 include ('_header.php');
 ?>
-<div class="container main">Spend</div>
+<div class="container main">
+	<h1>Visitor Spend</h1>
+	<h2>Visitor Numbers</h2>
+	<div class="row">
+		
+		<div class="col-md-4">Visitor Number</div>
+		<div class="col-md-4">Average Night</div>
+		<div class="col-md-4">Spend</div>
+		
+	</div>
+	<div>
+		Chart...
+	</div>
+</div>
 <?php
 include ('_footer.php');
 ?>
